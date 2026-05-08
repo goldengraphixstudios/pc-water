8322f3ebcf5d15ba525c108589e548c49e255204
goldengraphixstudios <goldengraphixstudios@gmail.com>
2026-05-08 08:55:50 +0800

    feat: add Tawk.to site-wide chat embed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import SiteChrome from '@/components/SiteChrome'
 
@@ -77,6 +78,20 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
       <body className="min-h-screen flex flex-col">
         <SiteChrome>{children}</SiteChrome>
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+            (function() {
+              var s1 = document.createElement("script");
+              var s0 = document.getElementsByTagName("script")[0];
+              s1.async = true;
+              s1.src = "https://embed.tawk.to/69fd33e94960f31c3616ee62/1jo2h5ibk";
+              s1.charset = "UTF-8";
+              s1.setAttribute("crossorigin", "*");
+              s0.parentNode.insertBefore(s1, s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   )
